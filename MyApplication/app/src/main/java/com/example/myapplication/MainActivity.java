0c124c876222f2c67f227402075638ab3109c6da
shaynexi <2064452932@qq.com>
2016-02-26 17:16:35 +0800

    完善页面返回

--- MyApplication/app/src/main/java/com/example/myapplication/MainActivity.java
+++ MyApplication/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 import java.util.regex.Matcher;
@@ -13,6 +14,7 @@ import java.util.regex.Pattern;
 public class MainActivity extends AppCompatActivity {
 
     private EditText et;
+    private Button btn;
 
 
     @Override
@@ -21,6 +23,16 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         et = (EditText) findViewById(R.id.editText);
 
+        btn = (Button) findViewById(R.id.button4);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setAction(Intent.ACTION_MAIN);
+                intent.addCategory(Intent.CATEGORY_HOME);
+                startActivity(intent);
+            }
+        });
     }
 
     public void onClick_Event(View v) {
@@ -37,11 +49,10 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void Register(View v)  {
-        Intent intent = new Intent();
-        Bundle bundle = new Bundle();
-        intent.setClass(MainActivity.this, RegisterActivity.class);
-        intent.putExtras(bundle);
+        Intent intent = new Intent(MainActivity.this, RegisterActivity.class);
         startActivity(intent);
         Log.d("Register", "register");
     }
+
+
 }
--- MyApplication/app/src/main/java/com/example/myapplication/RegisterActivity.java
+++ MyApplication/app/src/main/java/com/example/myapplication/RegisterActivity.java
@@ -3,7 +3,9 @@ package com.example.myapplication;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
@@ -13,6 +15,7 @@ import java.util.regex.Pattern;
 public class RegisterActivity extends AppCompatActivity {
 
     private EditText et3, et4, et5;
+    private Button btn;
 
 
     @Override
@@ -22,6 +25,15 @@ public class RegisterActivity extends AppCompatActivity {
         et3 = (EditText) findViewById(R.id.editText3);
         et4 = (EditText) findViewById(R.id.editText4);
         et5 = (EditText) findViewById(R.id.editText5);
+
+        btn = (Button) findViewById(R.id.button5);
+        btn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(RegisterActivity.this,MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
     public void Register_Check(View v) {
